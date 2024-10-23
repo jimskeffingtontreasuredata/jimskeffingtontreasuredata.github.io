f1ac5c79c381351fbe48cf3344ef2279486d5e7f
jimskeffingtontreasuredata <95382640+jimskeffingtontreasuredata@users.noreply.github.com>
2024-10-23 16:06:33 -0600

    Update handle_contact_form_submit.php

--- handle_contact_form_submit.php
+++ handle_contact_form_submit.php
@@ -1,16 +1,17 @@
 <?php
+session_start(); // Start the session to access session variables
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the submitted data
+    $firstName = htmlspecialchars($_POST['html_first_name']); // Sanitize input
+
+    // Here you can add code to handle the data, like saving it to a database or a file.
     
+    // Set a success message in the session
+    $_SESSION['success_message'] = "Thank you, $firstName! Your message has been received.";
 
-
-// collect data  
-    $php_first_name = $_POST['html_first_name'];
-    $php_referrer = $_SERVER['HTTP_REFERER'];
-    
-// prove it
-  echo 
-    "<script> 
-        alert('Thanks, $php_first_name!  We will be in touch soon.');
-        location.href = '$php_referrer';
-    </script>";
-
+    // Redirect back to the form page
+    header("Location: your_form_page.php"); // Change to the actual name of your form page
+    exit();
+}
 ?>
